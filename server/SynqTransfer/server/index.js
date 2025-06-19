f10e7bd9333861e39ca6bf9523de4c339d1a8e2b
ashishvrsiis <ashish@vrsiis.com>
2025-06-19 22:07:46 +0530

    Fixed the url issues

--- server/SynqTransfer/server/index.js
+++ server/SynqTransfer/server/index.js
@@ -29,7 +29,6 @@ app.use(cors({
 app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ extended: true, limit: '500mb' }));
 
-app.use(express.json());
 app.use(checkOrigin);
 app.use("/uploads", express.static("uploads"));
 app.use("/api/files", fileRoutes);
