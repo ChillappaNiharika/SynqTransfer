b7b313e3cbf1d61f27be3f9ae983978b213f7ef6
ashishvrsiis <ashish@vrsiis.com>
2025-06-19 22:04:32 +0530

    Fixed the origincheck

--- server/SynqTransfer/server/middlewares/originCheck.js
+++ server/SynqTransfer/server/middlewares/originCheck.js
@@ -1,11 +1,11 @@
 // middlewares/originCheck.js
 module.exports = function checkOrigin(req, res, next) {
-  const allowedOrigin = process.env.ALLOWED_ORIGINS?.split(',').map(origin => origin.trim()) || []; // Replace with your domain
+  const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',').map(origin => origin.trim()) || [];
   const origin = req.get("origin") || req.get("referer");
 
-  if (origin && origin.startsWith(allowedOrigin)) {
-    next();
-  } else {
-    return res.status(403).json({ error: "Access denied: Invalid origin" });
+  if (!origin || allowedOrigins.some(allowed => origin.startsWith(allowed))) {
+    return next();
   }
+
+  return res.status(403).json({ error: "Access denied: Invalid origin" });
 };
